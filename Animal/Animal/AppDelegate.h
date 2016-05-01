c79cd400a82288dd359497ede9b429ddc7a4be27
zb <zb@163.com>
2016-05-02 03:34:01 +0800

    初始化了Animal类

---
+++ Animal/Animal/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Animal
+//
+//  Created by zhangbin on 16/5/2.
+//  Copyright © 2016年 zhangbin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Animal/Animal/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Animal
+//
+//  Created by zhangbin on 16/5/2.
+//  Copyright © 2016年 zhangbin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
