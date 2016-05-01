2d86d5ea8a3826aa65e0615ee955d37c4e09f025
zb <zb@163.com>
2016-05-02 03:03:56 +0800

    创建了Person类

---
+++ XM/XM/PERSON.h
@@ -0,0 +1,13 @@
+//
+//  PERSON.h
+//  XM
+//
+//  Created by zhangbin on 16/5/2.
+//  Copyright © 2016年 zhangbin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PERSON : NSObject
+
+@end
---
+++ XM/XM/PERSON.m
@@ -0,0 +1,13 @@
+//
+//  PERSON.m
+//  XM
+//
+//  Created by zhangbin on 16/5/2.
+//  Copyright © 2016年 zhangbin. All rights reserved.
+//
+
+#import "PERSON.h"
+
+@implementation PERSON
+
+@end
